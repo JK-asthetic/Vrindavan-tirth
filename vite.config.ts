3e248c482e2fcc7e97a3f681ce88ea869426f7d1
JK-asthetic <khetanjatin12@gmail.com>
2025-08-21 10:37:24 +0530

    "Final"

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
-  base:"/Vrindavan-tirth",
+  base:"./",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
